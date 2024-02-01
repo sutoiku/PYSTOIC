7f22cd86bdd01e5082b1944f7d91e8692ac5b2f6
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2024-02-01 23:28:40 +0100

    chore: rename package to `PYSTOIC`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 setup(
-    name="pybook_setup",
+    name="PYSTOIC",
     version="0.1.0",
     packages=find_packages(),
     install_requires=Path("requirements.txt").read_text().splitlines(),
