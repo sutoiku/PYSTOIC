6e45a7f417f54b98ce682ccda74e87b8becf58fe
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2024-02-01 23:55:49 +0100

    chore: bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name="PYSTOIC",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=Path("requirements.txt").read_text().splitlines(),
     python_requires=">=3.10",
